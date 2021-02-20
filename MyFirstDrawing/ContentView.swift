9471ad67a901987a09b0335ab9032df860fcb351
Pedro Luis <pedroluisnr22@gmail.com>
2021-02-20 11:02:51 -0500

    Initial Commit

---
+++ MyFirstDrawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirstDrawing
+//
+//  Created by Pedro Luis on 2/20/21.
+//  Copyright © 2021 Pedro Luis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
